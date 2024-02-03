b447a8b8c8480ff9e2efdad4ed61d8fe6a201c9f
Daniel Vaughn <danielbvaughn@gmail.com>
2024-02-03 00:24:13 -0500

    fixes styles for help extension

---
+++ canvas/test.js
@@ -0,0 +1,6 @@
+
+let buffer = new ArrayBuffer(16)
+
+let view = new Int32Array(buffer)
+
+view[0] = 12345
--- extensions/help/Help.jsx
+++ extensions/help/Help.jsx
@@ -1,74 +1,84 @@
 const selectCommands = [
   {
     name: 'Select previous sibling',
+    tooltip: 'up',
     keys: ['↑'],
   },
   {
     name: 'Select next sibling',
+    tooltip: 'down',
     keys: ['↓'],
   },
   {
     name: 'Select parent',
+    tooltip: 'left',
     keys: ['←'],
   },
   {
     name: 'Select first child',
+    tooltip: 'right',
     keys: ['→'],
   },
   {
     name: 'Expand selection to previous sibling',
+    tooltip: 'shift + up',
     keys: ['⇧', '↑'],
   },
   {
     name: 'Expand selection to next sibling',
+    tooltip: 'shift + down',
     keys: ['⇧', '↓'],
   },
   {
     name: 'Select last sibling',
+    tooltip: 'cmd + down',
     keys: ['⌘', '↓'],
   },
   {
     name: 'Select first sibling',
+    tooltip: 'cmd + up',
     keys: ['⌘', '↑'],
   },
   {
     name: 'Save',
+    tooltip: 'cmd + s',
     keys: ['⌘', 's'],
   },
   {
     name: 'Select all siblings',
+    tooltip: 'cmd + a',
     keys: ['⌘', 'a'],
   },
 ]
 
 const Help = () => (
   <div
-    className="absolute top-0 left-0 right-0 z-10 flex items-center justify-center w-screen h-screen"
+    className="z-10 w-screen h-screen"
     onClick={() => {
       window.parent.postMessage({ action: 'exit_extension', data: {} })
     }}
   >
     <div
-      className="p-5 overflow-x-hidden rounded shadow bg-slate-800 text-slate-300 max-h-96"
+      className="p-5 overflow-x-hidden bg-slate-800 text-slate-300"
       onClick={(e) => e.stopPropagation()}
     >
       <h1 className="pb-1 mb-5 text-lg font-semibold text-white border-b border-dashed border-slate-600">Keyboard Shortcuts</h1>
 
       <h2 className="mb-3 text-sm font-semibold text-slate-50">Select Mode</h2>
 
-      <ul className="flex flex-col gap-2 text-sm select-none">
-        {selectCommands.map(({ name, keys }, i) => (
+      <ul className="flex flex-col gap-2 text-xs select-none">
+        {selectCommands.map(({ name, keys, tooltip }, i) => (
           <li
             key={`${name}_${i}`}
             className="flex justify-between"
           >
             <span className="mr-12">{name}</span>
 
-            <ul className="flex items-center gap-1">
+            <ul className="flex items-center gap-1" title={tooltip}>
               {keys.map((key, y) => (
                 <li
                   key={`key_${name}_${y}`}
-                  className="block w-6 p-1 leading-none text-center rounded-sm bg-slate-600"
+                  className="block w-6 p-1 leading-none text-center rounded-sm bg-slate-600 pointer-events-none"
                 >
                   {key}
                 </li>
