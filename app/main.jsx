a42c5251f3024ae076216fd12dc0bc2331acd47a
Daniel Vaughn <danielbvaughn@gmail.com>
2024-02-20 21:38:50 -0500

    removes old references

--- app/main.jsx
+++ app/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
-// import unsaved from './unsaved.png'
-// import originalFavicon from './favicon.svg'
 import './index.css'
 
 const reactRoot = ReactDOM.createRoot(document.getElementById('root'))
