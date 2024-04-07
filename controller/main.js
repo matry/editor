17258c143f842fb3d33ba359a8717181496b1224
Daniel Vaughn <danielbvaughn@gmail.com>
2024-04-07 12:23:43 -0400

    updates main page styles

--- controller/main.js
+++ controller/main.js
@@ -5,4 +5,5 @@ canvasIframe.onload = () => {
   initializeApp()
 }
 
+// this is done so that we can guarantee to receive the onload event. If the src is defined in the html, it introduces a race condition.
 canvasIframe.setAttribute('src', 'canvas/index.html')
