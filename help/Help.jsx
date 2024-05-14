17b69180d45764bea4a81fbf7bb01832b852d6d0
Daniel Vaughn <danielbvaughn@gmail.com>
2024-05-13 22:19:00 -0400

    updates help instruction for images

--- help/Help.jsx
+++ help/Help.jsx
@@ -30,7 +30,7 @@ const editorCommands = [
       name: 'Toggle box model overlay',
       tooltip: 'up',
       keys: ['t', '›', 'b'],
-    }
+    },
   ],
 ]
 
@@ -113,9 +113,9 @@ const editCommands = [
       keys: ['e', '›', 't'],
     },
     {
-      name: 'Edit image randomly',
+      name: 'Edit image',
       tooltip: 'up',
-      keys: ['e', '›', 'i', '›', 'r'],
+      keys: ['e', '›', 'i'],
     },
   ],
 ]
