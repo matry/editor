65263c1cef91e0231ae49007107be9b1f93c6b33
Daniel Vaughn <danielbvaughn@gmail.com>
2024-05-13 21:55:55 -0400

    moves randomId to node-compatible module

--- api/image-search.ts
+++ api/image-search.ts
@@ -1,6 +1,23 @@
 import axios from 'axios'
 import url from 'url'
-import { randomId } from '../controller/utils'
+
+function randomId (prefix = 'id', postfix = '', length = 8) {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('')
+
+  if (!length) {
+    length = Math.floor(Math.random() * chars.length)
+  }
+
+  let str = ''
+  for (let i = 0; i < length; i++) {
+    str += chars[Math.floor(Math.random() * chars.length)]
+  }
+
+  const pre = prefix ? `${prefix}_` : ''
+  const post = postfix ? `_${postfix}` : ''
+
+  return `${pre}${str}${post}`
+}
 
 export const config = {}
 
