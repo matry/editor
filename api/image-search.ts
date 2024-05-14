c1f2a64b292345f8450eb9b1cf6fefa2f938b0a9
Daniel Vaughn <danielbvaughn@gmail.com>
2024-05-13 20:43:03 -0400

    adds test for env vars

--- api/image-search.ts
+++ api/image-search.ts
@@ -1,32 +1,6 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export const config = {
-  runtime: 'edge',
-}
+export const config = {}
 
-export default async function handler(request: VercelRequest, response: VercelResponse) {
-  const { name = 'unknown person' } = request.query
-
-  return response.send(`Hello ${name}!`)
-
-  // let body;
-  // try {
-  //   body = await request.json();
-  // } catch (e) {
-  //   body = null;
-  // }
-
-  // if (body === null) {
-  //   return new Response(null, { status: 400, headers: { 'content-type': 'application/json' } })
-  // }
-
-  // return new Response(
-  //   JSON.stringify({ a: 1, b: 2 }),
-  //   {
-  //     status: 200,
-  //     headers: {
-  //       'content-type': 'application/json',
-  //     }
-  //   }
-  // )
+export function GET() {
+  return new Response(`Hello from ${process.env.IMAGE_SEARCH_CX}`);
 }
