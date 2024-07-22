889505972ccd976f910b947796da338f33e2c8b3
Daniel Vaughn <danielbvaughn@gmail.com>
2024-07-21 23:33:03 -0400

    improves loading state for image search

--- extensions/image/ImageSearch.jsx
+++ extensions/image/ImageSearch.jsx
@@ -55,7 +55,7 @@ export default function ImageSearch() {
           }
         }}
 
-        className="text-white h-full p-3"
+        className="text-white h-full p-3 flex flex-col"
       >
         <input
           id={IMAGE_SEARCH_ID}
@@ -72,7 +72,18 @@ export default function ImageSearch() {
         <input type="submit" className="hidden" />
 
         {isLoading && (
-          <div className="w-full h-full items-center flex justify-center text-2xl text-white">loading</div>
+          <div className="w-full h-full flex-grow items-center flex justify-center text-2xl text-white">
+            <div className="w-14 h-14 relative">
+              <svg
+                className="w-14 h-14 align-middle fill-current overflow-hidden animate-spin"
+                viewBox="0 0 1024 1024"
+              >
+                <path
+                  d="M512.056 908.647c-84.516 0-166.416-27.084-235.266-78.637-84.15-63.028-138.741-155.109-153.675-259.2-14.934-104.119 11.559-207.816 74.588-291.994 130.162-173.812 377.438-209.25 551.194-79.172 72.844 54.562 124.819 133.228 146.391 221.484 3.684 15.103-5.569 30.319-20.644 34.003-15.075 3.572-30.319-5.541-34.003-20.644-18.45-75.628-63-143.044-125.466-189.816-148.866-111.516-360.844-81.112-472.444 67.866-54.028 72.141-76.725 161.016-63.9 250.256 12.797 89.241 59.597 168.131 131.737 222.131 149.006 111.656 360.956 81.197 472.5-67.781 29.194-39.009 49.219-82.716 59.456-129.938 3.319-15.188 18.366-24.834 33.441-21.544 15.188 3.291 24.834 18.281 21.544 33.441-12.009 55.181-35.353 106.2-69.413 151.762-63.028 84.15-155.109 138.769-259.256 153.675-18.984 2.756-37.941 4.106-56.784 4.106z"
+                />
+              </svg>
+            </div>
+          </div>
         )}
 
         {!isLoading && (
